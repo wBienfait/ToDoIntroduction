6c75d26ce7492ad271933f31f14a0193479e2eed
wBienfait <wbienfait@gmail.com>
2018-09-13 09:47:11 +0200

    first files commit C#

---
+++ ConsoleApp2/Repository/IContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2.Repository
+{
+	public interface IContext<T> where T : CardEntity
+	{
+		List<T> EntityList();
+		List<T> EntityList(CardState state);
+	}
+}
---
+++ ConsoleApp2/Repository/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2.Repository
+{
+	interface IRepository <T> where T : CardEntity
+	{
+		T GetById(int id);
+		void Create(T entity);
+		void Delete(T entity);
+		void Update(T entity);
+		int GetNewId();
+	}
+}
